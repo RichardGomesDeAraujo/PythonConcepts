22e57dfe07226fbca51a463b9791e5dae2fc08cb
RichardGomesDeAraujo <richard.araujo74@yahoo.com>
2024-02-05 00:19:24 -0300

    Exemplo de Conexão

---
+++ ConnetionSQLServer.py
@@ -0,0 +1,19 @@
+pip install pyodbc
+
+import pyodbc
+import pandas as pd
+
+dados_conexao = (
+    "Driver={SQL Server};"
+    "Server=HDLS0101;"
+    "Database=Cliente360"
+)
+
+conexao = pyodbc.connect(dados_conexao)
+print("Conexão Bem Sucedida")
+
+comando_sql="SELECT * FROM TbProdutos"
+
+dados=pd.read_sql(comando_sql, conexao)
+
+display(dados)
